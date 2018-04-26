3efd5739cc0ce0a9642f0edff0b10f77de231cc0
Christopher Chen <chen1385@gmail.com>
2018-04-25 18:16:03 -0700

    added fetchWeather function and installed/implemented axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
 const API_KEY = `442a896dc90499577d52afb690a40c7d`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`
+
+    const request = axios.get(url);
+
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
