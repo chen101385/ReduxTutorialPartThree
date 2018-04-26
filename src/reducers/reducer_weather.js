5c5cc584c9f772d2f0ac5455a2a189d51732ca93
Christopher Chen <chen1385@gmail.com>
2018-04-26 14:43:28 -0700

    created weather_list container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,14 @@
-export default function(state = [], action) {
+import { FETCH_WEATHER } from '../actions/index'
+
+export default function (state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+        //NEVER MANIPULATE STATE IN REACT AND/OR REDUX;
+        //combine old array (state) & new array
+            //[OLD]return state.concat([action.payload.data])
+            //[NEW] using "spread syntax";
+            return [action.payload.data, ...state];
+    }
+
     return state;
 }
