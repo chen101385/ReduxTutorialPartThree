cf43490db992a53deb5874ffe4f8ef01511d46af
Christopher Chen <chen1385@gmail.com>
2018-04-27 12:27:04 -0700

    added google map functionality & converted temp to F from K

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
+
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const { lon, lat } = cityData.city.coord;
+        const tempsInK = cityData.list.map(weather => weather.main.temp)
+        const temps = tempsInK.map(K => K*(9/5) - 459.67)
+        const humidities = cityData.list.map(weather => weather.main.humidity)
+        const pressures = cityData.list.map(weather => weather.main.pressure)
+
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lng={lon} lat={lat} /></td>
+                <td><Chart data={temps} color="orange" units="F"/></td>
+                <td><Chart data={pressures} color="green" units="hPa"/></td>
+                <td><Chart data={humidities} color="blue" units="%"/></td>
             </tr>
         )
     }
@@ -17,9 +30,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
